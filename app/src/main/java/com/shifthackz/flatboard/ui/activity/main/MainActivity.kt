4c22dd5f1c85cb1bbe787fda7489ee0ebf8d3419
ShiftHackZ <dmitriy@moroz.cc>
2021-10-18 22:43:02 +0300

    Update

--- app/src/main/java/com/shifthackz/flatboard/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/shifthackz/flatboard/ui/activity/main/MainActivity.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import com.shifthackz.flatboard.ui.fragment.main.MainFragment
 import com.shifthackz.flatboard.databinding.ActivityMainBinding
 import com.shifthackz.flatboard.ui.base.BaseActivity
+import com.shifthackz.flatboard.utils.Constants.MAIN_SCREEN_TITLE
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -13,7 +14,7 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        supportActionBar?.title = "\uD835\uDC6D\uD835\uDC90\uD835\uDC8F\uD835\uDC95\uD835\uDC83\uD835\uDC90\uD835\uDC82\uD835\uDC93\uD835\uDC85"
+        supportActionBar?.title = MAIN_SCREEN_TITLE
         supportFragmentManager.beginTransaction().replace(
             binding.mainContainer.id,
             MainFragment.newInstance()
--- app/src/main/java/com/shifthackz/flatboard/utils/Constants.kt
+++ app/src/main/java/com/shifthackz/flatboard/utils/Constants.kt
@@ -3,4 +3,6 @@ package com.shifthackz.flatboard.utils
 object Constants {
     const val REQUEST_CODE_FONT_DETAIL = 1
     const val REQUEST_CODE_NUMERIC_DETAIL = 2
+
+    const val MAIN_SCREEN_TITLE = "\uD835\uDC6D\uD835\uDC90\uD835\uDC8F\uD835\uDC95\uD835\uDC83\uD835\uDC90\uD835\uDC82\uD835\uDC93\uD835\uDC85"
 }
